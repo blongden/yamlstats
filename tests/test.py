5f870a905dc7225c69f983eb534e98dd341a0c06
James Holdsworth <jwholdsworth@gmail.com>
2019-09-19 16:09:19 +0100

    Added tests for file differences

--- tests/test.py
+++ tests/test.py
@@ -5,9 +5,6 @@ import unittest
 class TestYamlStats(unittest.TestCase):
 
     def test_name_is_not_duplicated_in_files_1_and_2(self):
-        # arrange
-        ys = yamlstats.YamlStats()
-        ys.load('examples/file1.yaml', 'examples/file2.yaml')
         # act
         response = yamlstats.run('examples/file1.yaml', 'examples/file2.yaml')
 
@@ -20,3 +17,24 @@ class TestYamlStats(unittest.TestCase):
 
         # assert
         self.assertIn("name is the same in both files", response)
+
+    def test_name_has_different_values_in_files_1_and_3(self):
+        # arrange
+        ys = yamlstats.YamlStats()
+        ys.load('examples/file1.yaml', 'examples/file3.yaml')
+        # act
+        response = ys.check_for_differences()
+
+        # assert
+        self.assertIn({'file_a': {'file_name': 'examples/file1.yaml', 'value': 'Martin Devloper'}, 'key': 'name',
+                       'file_b': {'file_name': 'examples/file3.yaml', 'value': "Martin D'vloper"}}, response)
+
+    def test_it_prints_out_a_table(self):
+        # arrange
+        ys = yamlstats.YamlStats()
+
+        # act
+        response = ys.run('examples/file1.yaml', 'examples/file3.yaml', True)
+
+        # assert
+        self.assertIn('examples/file1.yaml', response.__str__())
